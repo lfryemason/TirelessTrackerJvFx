d71887d92bf4578310419d4b243f38b2071b70b2
Liam Frye-Mason <laflemur@gmail.com>
2018-09-22 16:39:20 -0600

    - Added stub for Stat tests.

---
+++ test/StatTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//TODO: Tests. edge cases, e.g. no item match list, 20000 match list.
+class StatTest {
+    @Test
+    void generateStats()
+    {
+    }
+
+    @Test
+    void addMatch() {
+    }
+
+    @Test
+    void removeMatch() {
+    }
+
+}
